3b7b1c618fa5c5fa796a747d14e5c2c27a0e6edb
nicholashelco <nicholashelco@gmail.com>
2023-04-15 11:24:00 -0400

    added CNN acceleration and facial feature mapping examples

--- FaceTestCNN.py
+++ FaceTestCNN.py
@@ -1,13 +1,16 @@
 from PIL import Image
 import face_recognition
 
+
 # Load the jpg file into a numpy array
 image = face_recognition.load_image_file("biden.jpg")
 
-# Find all the faces in the image using the default HOG-based model.
-# This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
-# See also: find_faces_in_picture_cnn.py
-face_locations = face_recognition.face_locations(image)
+# Find all the faces in the image using a pre-trained convolutional neural network.
+# This method is more accurate than the default HOG model, but it's slower
+# unless you have an nvidia GPU and dlib compiled with CUDA extensions. But if you do,
+# this will use GPU acceleration and perform well.
+# See also: find_faces_in_picture.py
+face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="cnn")
 
 print("I found {} face(s) in this photograph.".format(len(face_locations)))
 
@@ -21,5 +24,3 @@ for face_location in face_locations:
     face_image = image[top:bottom, left:right]
     pil_image = Image.fromarray(face_image)
     pil_image.show()
-
-    ##pil_image.save("myafce.png")
